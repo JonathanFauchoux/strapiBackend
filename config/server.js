07feac26f0dd79f32d06d9baa19a0aa1dbf9bc80
Joe <jonathan.fauchoux@gmail.com>
2020-09-10 14:29:55 +0200

    database/ server

--- config/server.js
+++ config/server.js
@@ -1,9 +1,11 @@
+//  First generate an environment variable STRAPI_PORT with the port you want strapi to run on.
+//ex: set STRAPI_PORT=1337
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT',`${process.env.PORT}`),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b1656e3f0aac4b041dbd3bba15826c80'),
+      secret: env('ADMIN_JWT_SECRET', '511cdebba0d21cc8b0cae58dc28fd69f'),
     },
   },
 });
