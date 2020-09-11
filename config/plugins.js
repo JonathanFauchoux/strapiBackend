9fd631a9a6d42a4dd7c42ace6c554464cb43a742
Joe <jonathan.fauchoux@gmail.com>
2020-09-11 13:34:13 +0200

    testCloudinary

---
+++ config/plugins.js
@@ -0,0 +1,10 @@
+module.exports = ({env}) => ({
+  téléverser : {
+    fournisseur : 'cloudinary',
+    providerOptions : {
+      cloud_name : env ('CLOUDINARY_NAME'),
+      api_key : env ('CLOUDINARY_KEY'),
+      api_secret : env ('CLOUDINARY_SECRET'),
+    },
+  },
+});
